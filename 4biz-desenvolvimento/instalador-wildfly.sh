aa32d4dbc4ee9a41eefec1c0cb65fde132ef0155
douglas <douglas@atenas>
2021-11-26 14:03:19 -0300

    fx: ajusta documentação

---
+++ 4biz-desenvolvimento/instalador-wildfly.sh
@@ -0,0 +1,7 @@
+mkdir ~/programas
+cd ~/git/4biz-desenvolvimento/4biz-desenvolvimento/ferramentas/
+unzip WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM.zip
+mv WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM ~/programas/
+cd ~/programas/WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM/standalone/
+sudo chmod +x clear.sh
+sudo ./clear.sh
--- 4biz-desenvolvimento/instalador.sh
+++ 4biz-desenvolvimento/instalador.sh
@@ -7,14 +7,12 @@ sudo apt install openjdk-8-jdk
 snap install teams
 snap install discord
 sudo apt install maven
-sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-sudo apt install pgadmin4-desktop
 sudo apt install docker-compose
 ./instalador-maven.sh
 ./instalador-docker.sh
 ./instalador-eclipse.sh
 ./baixador-projeto.sh
+./instalador-wildfly.sh
 
 
 

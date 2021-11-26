a54d4cbd496bd327364da4494e68c5e44a31ec65
douglas <douglas@atenas>
2021-11-26 15:21:33 -0300

    fix: ajusta documentação

--- 4biz-desenvolvimento/instalador.sh
+++ 4biz-desenvolvimento/instalador.sh
@@ -13,7 +13,13 @@ sudo apt install docker-compose
 ./instalador-eclipse.sh
 ./baixador-projeto.sh
 ./instalador-wildfly.sh
-
+cd ~/programas/
+wget https://mirror.umd.edu/eclipse/oomph/epp/2021-09/R/eclipse-inst-jre-linux64.tar.gz
+tar -xzf eclipse-inst-jre-linux64.tar.gz
+cd eclipse-installer
+./eclipse-inst&
+echo "Instale o Eclipse EE"
+echo "Siga as instruções contidas no arquivo leia-me.wiki, na seção #configurando-o-projeto-no-eclipse" 
 
 
 

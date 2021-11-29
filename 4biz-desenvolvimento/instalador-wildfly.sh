576bd210525cb6cb5ec5922018c4ed56108881d1
douglas <douglas@atenas>
2021-11-29 11:20:04 -0300

    fix: ajusta documentação

--- 4biz-desenvolvimento/instalador-wildfly.sh
+++ 4biz-desenvolvimento/instalador-wildfly.sh
@@ -1,7 +1,13 @@
 mkdir ~/programas
+
 cd ~/git/4biz-desenvolvimento/4biz-desenvolvimento/ferramentas/
+
 unzip WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM.zip
+
 mv WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM ~/programas/
-cd ~/programas/WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM/standalone/
+mv ~/programas/WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM ~/programas/WILDFLY
+cd ~/programas/WILDFLY/standalone/
+
 sudo chmod +x clear.sh
+
 sudo ./clear.sh

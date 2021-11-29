da84b1886ed931e5e9666add8a941ae22bd5f000
douglas <douglas@atenas>
2021-11-29 19:56:42 -0300

    fix: ajusta instalador

--- 4biz-desenvolvimento/instalador-docker.sh
+++ 4biz-desenvolvimento/instalador-docker.sh
@@ -5,5 +5,7 @@ unzip docker.zip
 mv docker /opt/4biz/
 cd /opt/4biz/docker/postgres
 sudo docker-compose up -d
+sleep 20s
+sudo docker ps
 sudo docker exec -i postgres psql -U postgres < ~/git/4biz-desenvolvimento/4biz-desenvolvimento/configuracoes/create-postgres.sql
 sudo docker exec -i postgres pg_restore -U postgres -d dbcitsmart /home/dbcitsmart.backup

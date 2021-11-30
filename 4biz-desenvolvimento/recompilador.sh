a1a595aa6fe4458fc033435a94e67eac146e2c82
douglas <douglashiura@gmail.com>
2021-11-30 14:22:28 -0300

    fix: ajusta instalador

--- 4biz-desenvolvimento/recompilador.sh
+++ 4biz-desenvolvimento/recompilador.sh
@@ -1,4 +1,5 @@
 cd ~/git/citsmart-itsm-enterprise/
 mvn clean install -DskipTests
+rm -R ~/programas/WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM/standalone/deployments/*
 cp target/*.war ~/programas/WILDFLY\ -\ 12.0.0.FINAL\ -\ ITSM/standalone/deployments/
 cd ~/git/4biz-desenvolvimento/4biz-desenvolvimento
